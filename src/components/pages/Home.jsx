cf04ca042c237ee6aa9e47104a9c34aeafb391d9
KimDoHyeon <ehcjswo1@gmail.com>
2023-07-25 09:58:35 +0900

    07-25 bg-blue modify

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,18 +9,18 @@ import Unsplash from "../section/Unsplash";
 import Movies from "../section/Movies";
 
 const Home = () => {
-    return (
-        <>
-            <Contents>
-                <Slider attr={"slider__wrap nanum5"} />
-                <Intro attr={"intro__wrap Nanum5 section bg-blue"} />
-                <Portfolio attr={"port__wrap Nanum5 section center bg-blue"} />
-                <Youtube attr={"youtube__wrap Nanum5 section"} />
-                <Unsplash attr={"unsplash__wrap Nanum5 section bg-blue"} />
-                <Movies attr={"movies__wrap Nanum5 container section"} />
-            </Contents>
-        </>
-    );
+  return (
+    <>
+      <Contents>
+        <Slider attr={"slider__wrap nanum5"} />
+        <Intro attr={"intro__wrap Nanum5 section bg-blue"} />
+        <Portfolio attr={"port__wrap Nanum5 section center"} />
+        <Youtube attr={"youtube__wrap Nanum5 section  bg-blue"} />
+        <Unsplash attr={"unsplash__wrap Nanum5 section"} />
+        <Movies attr={"movies__wrap Nanum5 container section  bg-blue"} />
+      </Contents>
+    </>
+  );
 };
 
 export default Home;
